a1228de0b98c98c0ceb8cb8a81b0ef3381a52854
Neill <neill@bankit.es>
2020-10-31 12:29:35 +0100

    Created with Spring Initializr

---
+++ src/main/java/com/roche/assignment/commerce/backend/CommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.roche.assignment.commerce.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceBackendApplication.class, args);
+	}
+
+}
