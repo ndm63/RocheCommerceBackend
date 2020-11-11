77edd4327326d6544c2494ec80ad79370aecd89c
Neill <neill@bankit.es>
2020-11-11 02:41:02 +0100

    Add unit test for get product item details

---
+++ src/test/java/com/roche/assignment/commerce/backend/basket/ProductTest.java
@@ -0,0 +1,30 @@
+/**
+ * Roche Home Assignment
+ */
+package com.roche.assignment.commerce.backend.basket;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.google.common.collect.Lists;
+
+/**
+ * Test methods in Product
+ *
+ * @author Neill McQuillin (created by)
+ * @since 11 November 2020 (creation date)
+ */
+public class ProductTest {
+	@Test
+	void testGetItemDetails_valid_textCorrect() {
+		final Product objectUnderTest = new Product(0.54f, "apple", "delicious green apple",
+				Lists.newArrayList("skin", "pips", "flesh"));
+
+		final BasketTotalCalculator calc = new BasketTotalCalculator();
+
+		final String output = calc.getItemDetails(objectUnderTest);
+
+		Assertions.assertThat(output).isEqualTo(
+				"This is apple, with description: delicious green apple with ingredients: [skin, pips, flesh]");
+	}
+}
